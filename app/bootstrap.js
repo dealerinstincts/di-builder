f8f88830a0f60c8fd16279271f6f53926bd480e7
Jason Valdron <jason@dealerinstincts.com>
2014-07-17 12:38:49 +0000

    more info

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -31,7 +31,7 @@
             new winston.transports.Console({
                 colorize: true,
                 timestamp: true,
-                level: 'warning'
+                level: 'info'
             })
         ];
         var exceptionHandlers = [
